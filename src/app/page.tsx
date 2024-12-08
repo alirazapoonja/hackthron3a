ae4a227c6cd3aa56a5c77248fe62e5add171619c
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-12-09 01:28:36 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import Navbar1 from './components/navbar1'
+import Linkbar from './components/linkbar'
+import Header from './components/header'
+
+export default function (){
+  return (
+    <div>
+      <Navbar/>
+      <Navbar1/>
+      <Linkbar/>
+      <Header/>
+    </div>
+  )
+}
+
